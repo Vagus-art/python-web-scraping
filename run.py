b325ab9612ec4c2eae7a655d92cf5033d2fd9259
Vagus-art <agustin.favo@gmail.com>
2020-11-25 03:29:23 -0300

    add requeriments, add hello world google search

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 PATH = "chromedriver"
 driver = webdriver.Chrome(PATH)
 driver.get("https://google.com")
 print(driver.title)
+
+search = driver.find_element_by_name("q")
+search.send_keys("hello world")
+search.send_keys(Keys.RETURN)
+
+print(driver.title)
+
 driver.quit()
