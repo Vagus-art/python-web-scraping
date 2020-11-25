ab7d25785c9f7127c68924681e6c31381fab5266
Vagus-art <agustin.favo@gmail.com>
2020-11-25 04:15:41 -0300

    get google titles

--- run.py
+++ run.py
@@ -1,15 +1,25 @@
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-PATH = "chromedriver"
-driver = webdriver.Chrome(PATH)
+# prepare the option for the chrome driver
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+
+driver = webdriver.Chrome(options=options)
 driver.get("https://google.com")
 print(driver.title)
 
 search = driver.find_element_by_name("q")
-search.send_keys("hello world")
+
+search_str = sys.argv[1]
+
+search.send_keys(search_str)
 search.send_keys(Keys.RETURN)
 
-print(driver.title)
+titles = driver.find_elements_by_xpath("//a/h3");
+
+for title in titles:
+    print(title.text.rstrip())
 
 driver.quit()
