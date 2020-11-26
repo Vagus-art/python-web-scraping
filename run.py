3e3e1db3a57bf845a0552cefa393df95f11273f9
Vagus-art <agustin.favo@gmail.com>
2020-11-26 02:41:42 -0300

    options

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# prepare the option for the chrome driver
+# prepare options for chrome driver
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 
